a6cae499b3701c10947872ee77cbe2136f1ffe89
Vaidika Vadhvaniya <vaidika.vadhvaniya@lapn222331.local>
2023-02-09 14:26:07 +0530

    Fixed: added pre-script

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Example: Create assets folder
+mkdir android/app/src/main/assets
+
+# Example: Remove old bundle
+rm android/app/src/main/assets/index.android.bundle
+
+# Example: Create new bundle
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
