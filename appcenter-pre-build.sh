3891f43a711149b07f4cf1bc0ce0f552853a5c65
Vaidika Vadhvaniya <vaidika.vadhvaniya@lapn222331.local>
2023-02-21 11:03:26 +0530

    Fixed: IOS-Pipeline

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -7,4 +7,4 @@ mkdir android/app/src/main/assets
 rm android/app/src/main/assets/index.android.bundle
 
 # Example: Create new bundle
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+npm run build:prod
